efd71a3e9639f20b728ec4d0c0f99774415a1c17
Ahmad-Rizki21 <ahmad2005rizki@gmail.com>
2026-04-13 09:40:59 +0700

    Remove Seeder in databaseseeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RolePermissionSeeder::class,
-            RealCompanySeeder::class,
+            // RolePermissionSeeder::class,
+            // RealCompanySeeder::class,
         ]);
     }
 }
